70306a49b167ccc49108357064538ad06c4c22d3
kpk0616 <kpk0616@naver.com>
2023-07-27 20:11:02 +0900

    [Init] 프로젝트 Init

---
+++ GreenSound/GreenSound/GreenSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenSoundApp.swift
+//  GreenSound
+//
+//  Created by 박의서 on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct GreenSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
